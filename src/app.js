951f379ba4e7212ca59cdc354c2ccf089dabd4b6
abhaysaini001 <abhaysaini262002@gmail.com>
2025-05-11 09:54:43 +0530

    Error-Handling

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middleware/auth")
-app.use("/admin",adminAuth)
-
-app.get("/user",userAuth,(req,res)=>{
-     res.send("User data sent")
-});
-app.get("/admin/getAllData",(req,res)=>{
-     res.send("all data sent")
-});
-
-app.get("/admin/deleteUser",(req,res)=>{
-     res.send("User Deleted") 
+app.get("/getUserData", (req,res)=>{
+     try {
+            throw new Error("dvbzhjf")
+     res.send("User Data Sent")
+     } catch (error) {
+          res.status(500).send("Some Eroor Occured Contact Support Team")
+     }
+     //Logic of DB Call and get User Data
+   
 })
 
-
+app.use("/",(err,req,res,next)=>{
+if(err){
+     res.status(500).send("something went wrong");
+}
+});
 
 app.listen(3000, ()=>{
      console.log("server is succesfully load");
