128be956cc2eec3d4a41b58d0b6ab762a70e57b5
abhaysaini001 <abhaysaini262002@gmail.com>
2025-05-20 13:51:26 +0530

    schema-validations

--- src/app.js
+++ src/app.js
@@ -1,35 +1,90 @@
-const express = require ("express");
-const connectDB = require("./config/database")
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-const User = require("./models/user")
+const User = require("./models/user");
 
 app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  //  creating a new instance of the User Model
+  const user = new User(req.body);
 
-app.post("/signup",async (req,res)=>{
-  
-
-    //  creating a new instance of the User Model
-     const user = new User(req.body);
-          
-
-     try{
-       await user.save();
-       res.send("User Added Successfully")
-     }
-     catch(err){
-       res.status(400).send("Error saving the user:"+ err.message);
-     }
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
 });
+
+// Get USer by EMail
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(user);
+    }
+    // const users = await User.find({ emailId: userEmail });
+    // if (users.length === 0) {
+    //   res.status(404).send("User Not Found");
+    // } else {
+    //   res.send(users);
+    // }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//feed API - GET/feed  -get all the user from the database
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// delete the user from database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Update the data of the USER
+app.patch("/user", async(req,res)=>{
+  const userId = req.body.userId;
+  const data = req.body;
+  try{
+ const user = await User.findByIdAndUpdate({_id: userId},data, 
+  {returnDocument:"after",
+    runValidators:true,
+  })
+ console.log(user)
+res.send("User updated successfully")
+  }catch(err){
+res.status(400).send("Update Failed" + err.message);
+  }
+})
 
 connectDB()
   .then(() => {
     console.log("Database Connection established..");
-    app.listen(3000, ()=>{
-     console.log("server is succesfully load....");
-});
+    app.listen(3000, () => {
+      console.log("server is succesfully load....");
+    });
   })
   .catch((err) => {
-    console.error("Database Cannot be connected!!",err.message);
+    console.error("Database Cannot be connected!!", err.message);
   });
-
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,48 @@ const mongoose = require("mongoose");
 
 const userSchema =  new mongoose.Schema({
     firstName :{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50,
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value))
+                throw new Error("Gender data is Not Valid")
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg"
+    },
+    about :{
+        type:String,
+        default:"This is default value of user!"
+    },
+    skills:{
+        type:[String]
     }
+},{
+    timestamps:true,
 });
 
 
