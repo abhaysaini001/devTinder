c135046303825c518f965852594ab2776c3a3b84
abhaysaini001 <abhaysaini262002@gmail.com>
2025-05-20 17:33:42 +0530

    Exploring Validator Library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const userSchema =  new mongoose.Schema({
     firstName :{
@@ -15,10 +16,21 @@ const userSchema =  new mongoose.Schema({
         required:true,
         unique:true,
         lowercase:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error ("Invalid Email Address" + value)
+            }
+        }
     },
     password:{
         type:String,
         required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error ("Please Make Strong Password:" + value)
+            }
+        }
     },
     age:{
         type:Number,
@@ -33,7 +45,12 @@ const userSchema =  new mongoose.Schema({
     },
     photoUrl:{
         type:String,
-        default:"https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg"
+        default:"https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error ("Invalid Img Url:" +  value)
+            }
+        }
     },
     about :{
         type:String,
