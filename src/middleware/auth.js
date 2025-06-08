99ccf7902114bb585f8e68840e76bb105a57662e
abhaysaini001 <abhaysaini262002@gmail.com>
2025-06-08 18:26:44 +0530

    middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,15 +3,24 @@ const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
   try {
-    const { token } = req.cookies;
+    let token;
+
+    if (req.cookies?.token) {
+      token = req.cookies.token;
+    } else if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
     if (!token) {
       return res.status(401).send("Please Login!");
     }
 
-    const decodedObj = await jwt.verify(token, "DEV@TINDER$790");
+    const decodedObj = jwt.verify(token, "DEV@TINDER$790");
 
-    const { _id } = decodedObj;
-    const user = await User.findById(_id);
+    const user = await User.findById(decodedObj._id);
     if (!user) {
       throw new Error("User Not Found");
     }
@@ -19,12 +28,6 @@ const userAuth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (err) {
-    res.status(400).send("ERROR :" + err.message);
+    res.status(401).send("ERROR: " + err.message);
   }
-  // Validate the token
-  // Find the user
-};
-
-module.exports = {
-  userAuth,
 };
